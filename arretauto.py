ea1c39338f0618a536073f5dacbe399e5d789b87
maximaxxxx <130175982+maximaxxxx@users.noreply.github.com>
2025-03-07 09:22:48 +0100

    arret auyomatik

--- arretauto.py
+++ arretauto.py
@@ -1,8 +1,9 @@
-def on_forever():
     Kitronik_Move_Motor.set_ultrasonic_units(Kitronik_Move_Motor.Units.CENTIMETERS)
     if Kitronik_Move_Motor.measure() < 10:
         Kitronik_Move_Motor.stop()
         basic.pause(100)
-        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.REVERSE, 35)
-        basic.pause(500)
+        Kitronik_Move_Motor.move(Kitronik_Move_Motor.DriveDirections.REVERSE, 10)
+        basic.pause(1500)
+        Kitronik_Move_Motor.spin(Kitronik_Move_Motor.SpinDirections.LEFT, 10)
+        basic.pause(1500)
 basic.forever(on_forever)
